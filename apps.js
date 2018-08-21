2f27fdad1bfbbe82f4b51146e0694f238cce0866
Jessica Duell <duellrdh@gmail.com>
2018-08-20 22:31:16 -0400

    created variables

---
+++ apps.js
@@ -0,0 +1,12 @@
+let name = "Jessica";
+let career = "Softward Developer";
+let description = "I love to code!";
+let interests = ["coding", "quilting", "playing games", "movies", "animals"];
+let pastPositions = [{company: "Redwood Dental", title: "Dental Hygienist", work: "Assessed patients' oral health, cleaned the oral environment and recommended courses of treatment"}, {company: "Family and Cosmetic Dentistry", title: "Dental Hygienist", work:"Assessed patients' oral health, cleaned the oral environment and recommended courses of treatment"}, {company: "University of Michigan", title: "TA", work:"I helped students learn material, showed them how to solve problems and be able to apply what they were learning"}];
+let skills = ["HTML", "Quilter", "CSS", "JavaScript", "Java", "Python", "Oboe" ];
+
+
+//function displayPosition that formats data
+function displayPosition(data){
+
+}
