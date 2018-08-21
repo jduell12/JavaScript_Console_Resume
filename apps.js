e4c2b89c6fb23500c8de661094f3a1261665122d
Jessica Duell <duellrdh@gmail.com>
2018-08-21 19:16:02 -0400

    completed through interests

--- apps.js
+++ apps.js
@@ -1,12 +1,36 @@
-let name = "Jessica";
-let career = "Softward Developer";
-let description = "I love to code!";
+
 let interests = ["coding", "quilting", "playing games", "movies", "animals"];
+
 let pastPositions = [{company: "Redwood Dental", title: "Dental Hygienist", work: "Assessed patients' oral health, cleaned the oral environment and recommended courses of treatment"}, {company: "Family and Cosmetic Dentistry", title: "Dental Hygienist", work:"Assessed patients' oral health, cleaned the oral environment and recommended courses of treatment"}, {company: "University of Michigan", title: "TA", work:"I helped students learn material, showed them how to solve problems and be able to apply what they were learning"}];
+
 let skills = ["HTML", "Quilter", "CSS", "JavaScript", "Java", "Python", "Oboe" ];
+
+let resume = {
+    name: "Jessica",
+    career: "Software Developer",
+    description: "I love to code!",
+};
 
 
 //function displayPosition that formats data
-function displayPosition(data){
-
+function displayPosition (company, title, work){
+    console.log(" * " + title + " at " + company + " - " + work);
 }
+
+//function that turns an array into a list with bullet points
+function list (arr){
+    for (let i = 0; i < interests.length; i++){
+        console.log(" * " + interests[i]);
+    }
+};
+
+console.log("Name: " + resume.name.toUpperCase());
+console.log("Career: " + resume.career);
+console.log("Description: " + resume.description);
+console.log("\n");
+console.log("My Interests: ");
+list(interests);
+console.log("\n");
+console.log("My Previous Experiences: ");
+displayPosition(pastPositions.company)
+
